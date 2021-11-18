e78a8585ebe6eff524c6526bc7aa3424877d68b3
Riki-Toro <rikitoro22@gmail.com>
2021-11-18 11:55:34 +0700

    up

--- application/views/penduduk/index.php
+++ application/views/penduduk/index.php
@@ -3,7 +3,7 @@
         <div class="row">
 
             <div class="span8">
-                <table class="table table-striped table-hover">
+                <!-- <table class="table table-striped table-hover">
                     <thead>
                         <tr>
                             <th>No</th>
@@ -31,15 +31,37 @@
                             </tr>
                         <?php endforeach; ?>
                     </tbody>
-                </table>
+                </table> -->
 
                 <!-- new table -->
-                <table>
-                    <thead>
-
-                    </thead>
-
+                <table border="1" cellpadding="5">
+                    <!-- <thead>
+                        <tr>
+                            <th>NO<th>
+                            <th>JETIS<th>
+                            <th>L<th>
+                            <th>P<th>
+                            <th>KK<th>
+                            <th>W<th>
+                        </tr>
+                    </thead> -->
                     <tbody>
+                        <tr>
+                            <td>NO</td>
+                            <td>JETIS</td>
+                            <td>L</td>
+                            <td>P</td>
+                            <td>KK</td>
+                            <td>W</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Sehat <br> RT 02/RW 01</td>
+                            <td>49</td>
+                            <td>60</td>
+                            <td>36</td>
+                            <td>29</td>
+                        </tr>
                         
                     </tbody>
                 </table>
