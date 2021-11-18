6950226a8c684076f6306995302eb2e8b59973b7
Riki-Toro <rikitoro22@gmail.com>
2021-11-18 21:07:03 +0700

    update penduduk

--- application/views/penduduk/index.php
+++ application/views/penduduk/index.php
@@ -33,6 +33,7 @@
                     </tbody>
                 </table> -->
 
+                <p><i>*terakhir diperbaharui : Januari 2021</i></p><br>
                 <!-- new table -->
                 <h4><b>Dukuh Jetis</b></h4>
                 <table border="2" cellpadding="7" width="700">
@@ -65,34 +66,42 @@
                         </tr>
                         <tr>
                             <td>2</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Sunarji <br> RT 01/RW 01</td>
+                            <td>59</td>
+                            <td>74</td>
+                            <td>46</td>
+                            <td>34</td>
                         </tr>
                         <tr>
                             <td>3</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
+                            <td style="text-align:left;">Misbandi <br> RT 02/RW 02</td>
+                            <td>54</td>
+                            <td>45</td>
                             <td>36</td>
-                            <td>29</td>
+                            <td>25</td>
                         </tr>
                         <tr>
                             <td>4</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Jarmanto <br> RT 01/RW 02</td>
+                            <td>70</td>
+                            <td>62</td>
+                            <td>45</td>
+                            <td>33</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td style="text-align:left;">Miskun <br> RT 03/RW 02</td>
+                            <td>56</td>
+                            <td>56</td>
+                            <td>39</td>
+                            <td>34</td>
                         </tr>
                         <tr>
                             <td colspan="2">Jumlah</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td>288</td>
+                            <td>297</td>
+                            <td>202</td>
+                            <td>155</td>
                         </tr>
                     </tbody>
                 </table>
@@ -121,42 +130,42 @@
                         </tr>
                         <tr>
                             <td>1</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Sutopo <br> RT 02/RW 01</td>
+                            <td>69</td>
+                            <td>63</td>
+                            <td>43</td>
+                            <td>31</td>
                         </tr>
                         <tr>
                             <td>2</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Edin Waloyo <br> RT 01/RW 02</td>
+                            <td>63</td>
+                            <td>46</td>
+                            <td>42</td>
+                            <td>33</td>
                         </tr>
                         <tr>
                             <td>3</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Katenu <br> RT 02/RW 02</td>
+                            <td>70</td>
+                            <td>74</td>
+                            <td>52</td>
+                            <td>39</td>
                         </tr>
                         <tr>
                             <td>4</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Jauhari <br> RT 01/RW 01</td>
+                            <td>44</td>
+                            <td>46</td>
+                            <td>35</td>
+                            <td>28</td>
                         </tr>
                         <tr>
                             <td colspan="2">Jumlah</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td>246</td>
+                            <td>229</td>
+                            <td>172</td>
+                            <td>131</td>
                         </tr>
                     </tbody>
                 </table>
@@ -185,49 +194,58 @@
                         </tr>
                         <tr>
                             <td>1</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Sunardi <br> RT 01/RW 01</td>
+                            <td>39</td>
+                            <td>40</td>
+                            <td>30</td>
+                            <td>24</td>
                         </tr>
                         <tr>
                             <td>2</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Mukri <br> RT 02/RW 01</td>
+                            <td>56</td>
+                            <td>58</td>
+                            <td>42</td>
+                            <td>35</td>
                         </tr>
                         <tr>
                             <td>3</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Brian <br> RT 01/RW 02</td>
+                            <td>63</td>
+                            <td>65</td>
+                            <td>50</td>
+                            <td>39</td>
                         </tr>
                         <tr>
                             <td>4</td>
-                            <td style="text-align:left;">Sehat <br> RT 02/RW 01</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td style="text-align:left;">Uli W <br> RT 02/RW 02</td>
+                            <td>70</td>
+                            <td>59</td>
+                            <td>42</td>
+                            <td>32</td>
                         </tr>
                         <tr>
                             <td colspan="2">Jumlah</td>
-                            <td>49</td>
-                            <td>60</td>
-                            <td>36</td>
-                            <td>29</td>
+                            <td>228</td>
+                            <td>222</td>
+                            <td>164</td>
+                            <td>130</td>
                         </tr>
                     </tbody>
                 </table>
+                <br><br>
                 <!-- akhir new table -->
 
+                <p>Keterangan :</p>
+                <ul>
+                    <li>L : Laki-Laki</li>
+                    <li>P : Perempuan</li>
+                    <li>KK : Kartu Keluarga</li>
+                    <li>W : Wuwung</li>
+                </ul>
 
-                <br><br>
+
+                
                 <!-- <div id="pagination">
                     <span class="all">Page 1 of 3</span>
                     <span class="current">1</span>
