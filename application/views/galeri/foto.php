7eebc4c87bbe9811a70e52975d8179db185baf6f
Riki-Toro <rikitoro22@gmail.com>
2021-11-26 22:19:24 +0700

    up

--- application/views/galeri/foto.php
+++ application/views/galeri/foto.php
@@ -2,6 +2,10 @@
     <div class="container">
         <div class="row">
 
-            <div class="span8">
+            <!-- <div class="span8">
                 <h3>Tidak ada data foto!</h3>
+            </div> -->
+            <div class="span3">
+                <h3>Tidak ada data foto!</h3>
+                <img src="<?php echo base_url('public/'); ?>img/slides/camera/slide1/balaidesa.jpeg);" alt="">
             </div>
